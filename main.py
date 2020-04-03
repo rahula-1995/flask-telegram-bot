d6b2386230c780ffed4e72b420999f7356c232ce
Rahul <alokrockrahul@gmail.com>
2020-04-03 18:59:17 +0530

    k

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+#chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument('--disable-gpu')
+#chrome_options.add_argument('--no-sandbox')
+#chrome_options.binary_location = GOOGLE_CHROME_PATH
+#driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
-#service = Service('/Users/Dell pc/Desktop/chromedriver')
-#service.start()
-#driver = webdriver.Remote(service.service_url)
+service = Service(CHROMEDRIVER_PATH)
+service.start()
+driver = webdriver.Remote(service.service_url)
 app=Flask(__name__)
 def write_json(data ,filename='response.json'):
 	with open(filename ,'w') as f:
